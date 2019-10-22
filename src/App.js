84a8027616a745e1a61a9d15379f6eadd7e0d91e
unknown <recfrag@gmail.com>
2019-10-22 15:41:29 +0300

    fetch planets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    planets: []
+  }
+
+  componentDidMount(){
+
+      fetch(`https://swapi.co/api/planets/`)
+      .then(res => {
+        return res.json()
+      })
+      .then(({results}) => {
+        console.log(results)
+        this.setState({
+            planets: results
+        })
+        console.log(this.state.planets)
+
+      })
+      
+    }
+
+  render() {
+    return (
+     <div>
+       <h1>Planets</h1>
+       <div>
+         <img src="" alt=""/>
+         <p>title</p>
+         <p>diameter:</p>
+         <a href="" target="_blank">link</a>
+         
+       </div>
+     </div>
+    );
+  }
 }
 
-export default App;
+
+
+
