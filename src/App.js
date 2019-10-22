9f45c22ef2de6c40b3e9e47b86f2d667b135a424
unknown <recfrag@gmail.com>
2019-10-22 20:05:35 +0300

    add preloader

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import React, { Component } from 'react';
 export default class App extends Component {
 
   state = {
-    planets: []
+    planets: [],
+    loading: false
   }
 
   componentDidMount() {
+    this.setState({
+      loading: true
+    })
 
     fetch(`https://swapi.co/api/planets/`)
       .then(res => {
         return res.json()
       })
       .then(({ results }) => {
-        console.log(results)
         this.setState({
-          planets: results
+          planets: results,
+          loading: false
         })
-        console.log(this.state.planets)
 
       })
 
@@ -25,32 +28,37 @@ export default class App extends Component {
 
   render() {
 
-    const { planets } = this.state
-
-    return (
-      <div className="container">
-        <h1 className="header">Star Wars planets:</h1>
-        <div className="main">
-          {planets.map(planet => (
-            <div key={planet.url} className="planet">
-              <h3>name: {planet.name}</h3>
-              <span>diameter: {planet.diameter}</span>
-
-            </div>
-
-          ))}
-
-          {/* <div>
-         <img src="" alt=""/>
-         <p>title</p>
-         <p>diameter:</p>
-         <a href="" target="_blank">link</a>
-         
-       </div> */}
+    const { planets, loading } = this.state
+    if(loading){
+      return <div className="loading">Loading...</div>
+    }{
+      return (
+        <div className="container">
+          <h1 className="header">Star Wars planets:</h1>
+          <div className="main">
+            {planets.map(planet => (
+              <div key={planet.url} className="planet">
+                <h3>{planet.name}</h3>
+                <span>diameter: {planet.diameter}</span>
+  
+              </div>
+  
+            ))}
+  
+            {/* <div>
+           <img src="" alt=""/>
+           <p>title</p>
+           <p>diameter:</p>
+           <a href="" target="_blank">link</a>
+           
+         </div> */}
+          </div>
         </div>
-      </div>
+  
+      );
+    }
 
-    );
+    
   }
 }
 
